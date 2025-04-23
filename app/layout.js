2cce5206ac11bac651c7aca546ea598e6d535643
Brown Oziomachi <browncemmanuel@gmail.com>
2025-04-23 17:04:58 +0100

    layout

--- app/layout.js
+++ app/layout.js
@@ -1,18 +1,18 @@
 
-import { Poppins } from "next/font/google";
 import "./globals.css";
+import { Poppins } from "next/font/google"
 import Navbar from "@/components/Navbar";
 import AuthProvider from "@/components/AuthProvider";
 
-export const metadata = {
-  title: "Webwiz Creation",
-  description: "Explore creativity with Webwiz Creation.",
-};
-
+const PoppinsFont = Poppins({
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700'],
+  variable: '--font-poppins',
+})
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={`${Poppins.className} antialiased`}>
+    <html lang="en" className={PoppinsFont.variable}>
+      <body className={` antialiased`}>
         <AuthProvider>
           <Navbar />
           <main className="bg-gradient-to-br from-gray-900 via-black to-gray-800 min-h-screen text-white">
