0eaba84a16386a72b5e0882ad75b9af1efa20959
Brown Oziomachi <browncemmanuel@gmail.com>
2025-05-06 12:30:35 +0100

    update

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,15 +1,15 @@
 
 import { handlers } from "@/auth"
-export const { GET, POST } = handlers
 
 import NextAuth from "next-auth"
 import Facebook from "next-auth/providers/facebook"
 import GitHub from "next-auth/providers/github"
 import Google from "next-auth/providers/google"
 
+export const { GET, POST } = handlers
 export const {signIn, signOut, auth } = NextAuth({
-    providers: [GitHub],
     providers: [Google],
+    providers: [GitHub],
     providers: [Facebook],
 })
 
