6756476659b0685b8fe9d61426f8db9f6d3a40bd
Brown Oziomachi <browncemmanuel@gmail.com>
2025-07-29 09:21:20 +0100

    update

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,8 @@
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import AuthProvider from "@/components/AuthProvider";
-
-
+import { Analytics } from '@vercel/analytics/next';
+ 
 
 export const metadata = {
   title: "Webwiz Creation - The Sun Web",
@@ -21,17 +21,18 @@ export default function RootLayout({ children }) {
       <head>
         <meta name="google-adsense-account" content="ca-pub-8408243121163767" />
 
-      
         {/* Other meta tags */}
-       <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8408243121163767"
-     crossorigin="anonymous"></script>
-
-
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8408243121163767"
+          crossorigin="anonymous"
+        ></script>
       </head>
       <body className="antialiased">
         <AuthProvider>
           <Navbar />
           {children}
+          <Analytics />
         </AuthProvider>
       </body>
     </html>
