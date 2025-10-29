7e39d7692a6fa3f83ec5a35c76a0013e974b9e6e
Brown Oziomachi <browncemmanuel@gmail.com>
2025-10-29 23:52:55 +0100

    Add sitemap and robots.txt for SEO

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://thecyclopedia.com.ng',
+    generateRobotsTxt: true,
+    sitemapSize: 7000, // optional, controls splitting large sitemaps
+    changefreq: 'weekly',
+    priority: 0.7,
+};
